753a99d99d960cf096e169fb14f39da51dfc81d5
Abderrahim-belaid-123 <AbdeBelaid218@gmail.com>
2023-12-14 17:59:45 +0100

    personnalisation de la page d'erreur 404

--- routes/web.php
+++ routes/web.php
@@ -40,4 +40,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::fallback(function() {
+    return view('notfound.notfound'); // la vue 404.blade.php
+ });
+
 require __DIR__ . '/auth.php';
